84861a9a8ed4ee19b9d8561a466b8de57e2e7e60
SpectreOfSolitude <kakak16903@gmail.com>
2023-11-10 23:32:25 +0700

    Take Home Task 6 Final

--- TakeHomeTask - Tangent - 6/Pages/Home/script.js
+++ TakeHomeTask - Tangent - 6/Pages/Home/script.js
@@ -1,6 +1,5 @@
 function posting(){
     event.preventDefault();
-    console.log("ping")
     let post = document.getElementById("inputPost").value;
     console.log(post)
     let template = document.createElement("p")
@@ -9,8 +8,5 @@ function posting(){
     template.appendChild(textNode);
     feedsbox = document.getElementById("feeds");
     feedsbox.appendChild(template)
-
-    console.log(feedsbox)
-    console.log("p0ng")
 }
 
